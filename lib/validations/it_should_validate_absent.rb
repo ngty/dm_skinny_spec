8574f07ddf4b6cc695c0d923a3564085a4e3a925
NgTzeYang <ngty77@gmail.com>
2009-01-04 22:47:21 +0800

    Did tidying up.

--- lib/validations/it_should_validate_absent.rb
+++ lib/validations/it_should_validate_absent.rb
@@ -9,19 +9,24 @@ module DmSkinnySpec::Validations::ItShouldValidateAbsent
     include DmSkinnySpec::Validations::Common
 
     def validate_all( attr, context, opts )
-      err_msg = (opts[:message]||'%s must be absent').t(humanize_attr(attr))
+      err_msg = (opts[:message]||'%s must be absent').t(humanize(attr))
+      opts[:boolean] ?
+        validate_bool( attr, context, err_msg ) :
+        validate_str( attr, context, err_msg )
+    end
 
-      unless opts[:boolean]
-        validate_err_val( attr, 'x', context,
-          "should not be valid if :#{attr} is not blank" )
-        validate_err_msg( attr, 'x', err_msg, context,
-          "should have error \"#{err_msg}\" if :#{attr} is not blank" )
-      else
-        validate_err_val( attr, true, context,
-          "should not be valid if :#{attr} is true" )
-        validate_err_msg( attr, true, err_msg, context,
-          "should have error \"#{err_msg}\" if :#{attr} is true" )
-      end
+    def validate_bool( attr, context, err_msg )
+      validate_err_val( attr, true, context,
+        "should not be valid if :#{attr} is true" )
+      validate_err_msg( attr, true, err_msg, context,
+        "should have error \"#{err_msg}\" if :#{attr} is true" )
+    end
+
+    def validate_str( attr, context, err_msg )
+      validate_err_val( attr, 'x', context,
+        "should not be valid if :#{attr} is not blank" )
+      validate_err_msg( attr, 'x', err_msg, context,
+        "should have error \"#{err_msg}\" if :#{attr} is not blank" )
     end
 
   end
