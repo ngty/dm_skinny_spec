1f485b63e99ad9fadb4943451d88ff65ac0203d7
NgTzeYang <ngty77@gmail.com>
2009-01-04 22:49:36 +0800

    Removed validating of valid values (validating nil value still remains), and did some tidying up.

--- lib/validations/it_should_validate_is_accepted.rb
+++ lib/validations/it_should_validate_is_accepted.rb
@@ -11,31 +11,25 @@ module DmSkinnySpec::Validations::ItShouldValidateIsAccepted
     def validate_all( attr, context, opts )
       err_msg = (opts[:message]||'%s is not accepted').t(humanize(attr))
 
-      validate_valids( attr, context, opts[:valids] )
-      validate_invalids( attr, context, opts[:invalids], err_msg )
-      validate_nil( attr, context, opts[:allow_nil], err_msg )
+      validate_err_vals attr, context, opts[:reject], err_msg
+      validate_nil_val  attr, context, opts[:allow_nil], err_msg
     end
 
-    def validate_valids( attr, context, ok_vals )
-      ( ok_vals || [ '1', 1, true, 'true', 't' ] ).each do |ok_val|
-        validate_ok_val( attr, ok_val, context, 
-          "should be valid if :#{attr} is " + humanize(ok_val) )
-      end
-    end
-
-    def validate_invalids( attr, context, err_vals, err_msg )
+    def validate_err_vals( attr, context, err_vals, err_msg )
       ( err_vals || [ '0', 0, false, 'false', 'f' ] ).each do |err_val|
-        validate_err_val( attr, err_val, context, 
-          "should not be valid if :#{attr} is " + humanize(err_val) )
-        validate_err_msg( attr, err_val, err_msg, context, 
-          "should have error \"#{err_msg}\" if :#{attr} is " + humanize(err_val) )
+        validate_err_val attr, err_val, context, 
+          "should not be valid if :#{attr} is #{humanize(err_val)}"
+        validate_err_msg attr, err_val, err_msg, context, 
+          "should have error \"#{err_msg}\" if :#{attr} is #{humanize(err_val)}"
       end
     end
 
-    def validate_nil( attr, context, allow_nil, err_msg )
-      ( allow_nil.nil? ? true : allow_nil ) ?
-        validate_valids( attr, context, [nil] ) :
-        validate_invalids(  attr, context, [nil], err_msg )
+    def validate_nil_val( attr, context, allow_nil, err_msg )
+      if allow_nil == false
+        validate_err_vals attr, context, [nil], err_msg
+      else
+        validate_ok_val attr, nil, context, "should be valid if :#{attr} is nil"
+      end
     end
 
   end
