212e9e9fc849232b4219d0f81f14fed53ed6fbb9
NgTzeYang <ngty77@gmail.com>
2009-01-04 22:50:36 +0800

    Updated as humanize_attr() has been replaced by humanize().

--- lib/validations/it_should_validate_is_unique.rb
+++ lib/validations/it_should_validate_is_unique.rb
@@ -11,14 +11,14 @@ module DmSkinnySpec::Validations::ItShouldValidateIsUnique
     private
 
       def validate_all( attr, context, opts )
-        err_msg = (opts[:message]||'%s is already taken').t(humanize_attr(attr))
+        err_msg = (opts[:message]||'%s is already taken').t(humanize(attr))
         err_val = lambda { |eg| eg.instance(:other).attribute_get(attr) }
 
-        validate_err_val attr, err_val, context,
-          "should not be valid if :#{attr} is not unique"
+        validate_err_val( attr, err_val, context,
+          "should not be valid if :#{attr} is not unique" )
 
-        validate_err_msg attr, err_val, err_msg, context,
-          "should have error \"#{err_msg}\" if :#{attr} is not unique"
+        validate_err_msg( attr, err_val, err_msg, context,
+          "should have error \"#{err_msg}\" if :#{attr} is not unique" )
       end
   end
 
