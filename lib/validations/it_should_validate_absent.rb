9903f4df106d535ebcb219b22bb902f9d516e3e2
NgTzeYang <ngty77@gmail.com>
2009-01-07 00:24:41 +0800

    Tidied up as we improve DmSkinnySpec::Validations module.

--- lib/validations/it_should_validate_absent.rb
+++ lib/validations/it_should_validate_absent.rb
@@ -1,32 +1,21 @@
 module DmSkinnySpec::Validations::ItShouldValidateAbsent
 
-  def it_should_validate_absent( attr, opts={} )
-    DmSkinnySpec::Validations::ItShouldValidateAbsent.validate attr, opts, self
+  def it_should_validate_absent( attribute, options={} )
+    DmSkinnySpec::Validations::ItShouldValidateAbsent.run attribute, options, self
   end
 
   class << self
 
     include DmSkinnySpec::Validations::Common
 
-    def validate_all( attr, context, opts )
-      err_msg = (opts[:message]||'%s must be absent').t(humanize(attr))
-      opts[:boolean] ?
-        validate_bool( attr, context, err_msg ) :
-        validate_str( attr, context, err_msg )
-    end
+    def validate
+      @message ||= '%s must be absent'.t(@attribute_name)
 
-    def validate_bool( attr, context, err_msg )
-      validate_err_val( attr, true, context,
-        "should not be valid if :#{attr} is true" )
-      validate_err_msg( attr, true, err_msg, context,
-        "should have error \"#{err_msg}\" if :#{attr} is true" )
-    end
-
-    def validate_str( attr, context, err_msg )
-      validate_err_val( attr, 'x', context,
-        "should not be valid if :#{attr} is not blank" )
-      validate_err_msg( attr, 'x', err_msg, context,
-        "should have error \"#{err_msg}\" if :#{attr} is not blank" )
+      if @options[:boolean]
+        validate_error_values true, "%s"
+      else
+        validate_error_values 'x', "not blank"
+      end
     end
 
   end
--- lib/validations/it_should_validate_present.rb
+++ lib/validations/it_should_validate_present.rb
@@ -1,23 +1,20 @@
 module DmSkinnySpec::Validations::ItShouldValidatePresent
 
-  def it_should_validate_present( attr, opts={} )
-    DmSkinnySpec::Validations::ItShouldValidatePresent.validate attr, opts, self
+  def it_should_validate_present( attribute, options={} )
+    DmSkinnySpec::Validations::ItShouldValidatePresent.run attribute, options, self
   end
 
   class << self
 
     include DmSkinnySpec::Validations::Common
 
-    def validate_all( attr, context, opts )
-      is_bool = opts[:boolean]
-      opts[:message] ||= is_bool ? '%s must not be nil' : '%s must not be blank' 
-      err_msg = opts[:message].t(humanize(attr))
-
-      ( is_bool ? [nil] : [ nil, '' ] ).each do |err_val|
-        validate_err_val( attr, err_val, context,
-          "should not be valid if :#{attr} is " + humanize(err_val) )
-        validate_err_msg( attr, err_val, err_msg, context,
-          "should have error \"#{err_msg}\" if :#{attr} is " + humanize(err_val) )
+    def validate
+      if @options[:boolean]
+        @message ||= '%s must not be nil'.t(@attribute_name)
+        validate_error_values nil, '%s'
+      else
+        @message ||= '%s must not be blank'.t(@attribute_name)
+        validate_error_values [ nil, '' ], '%s'
       end
     end
 
