345b63381ed222debff0317df83a6ba6ce00dc6f
NgTzeYang <ngty77@gmail.com>
2009-01-08 09:22:52 +0800

    Did cleaning up.

--- lib/validations/it_should_validate_format.rb
+++ lib/validations/it_should_validate_format.rb
@@ -1,20 +1,20 @@
 module DmSkinnySpec::Validations::ItShouldValidateFormat
 
-  class UnexpectedFormat          < Exception ; end
-  class UndefinedValidationValues < Exception ; end
+  class UnexpectedFormat      < Exception ; end
+  class UndefinedRejectValues < Exception ; end
 
-  def it_should_validate_format( attr, opts={} )
-    validator = DmSkinnySpec::Validations::ItShouldValidateFormat
-    opts[:as] ||= opts.delete(:with)
+  def it_should_validate_format( attribute, options={} )
+    validator =  DmSkinnySpec::Validations::ItShouldValidateFormat
+    options[:as] ||= options.delete(:with)
 
-    case opts[:as] 
+    case options[:as]
       when :url, :email_address
-        validator.validate( attr, opts, self )
+        validator.run attribute, options, self
       when Symbol
         raise UnexpectedFormat
       else
-        opts[:reject].is_a?(Array) ?
-          validator.validate( attr, opts, self ) : raise(UndefinedValidationValues)
+        raise UndefinedRejectValues unless options[:reject]
+        validator.run attribute, options, self
     end
   end
 
@@ -22,65 +22,61 @@ module DmSkinnySpec::Validations::ItShouldValidateFormat
 
     include DmSkinnySpec::Validations::Common
 
-    # NOTE: these examples are lifted from dm-validations specs
-    
-    INVALID_EMAILS = [
-        '-- dave --@example.com', '[dave]@example.com', '.dave@example.com',
-        'Max@Job 3:14', 'Job@Book of Job', 
+    attr_writer :invalid_emails, :invalid_urls
+
+    def invalid_emails
+      @invalid_emails || [
+        # Lifted from dm-validations specs
+        '-- dave --@example.com', 
+        '[dave]@example.com', 
+        '.dave@example.com',
+        'Max@Job 3:14', 
+        'Job@Book of Job', 
         'J. P. \'s-Gravezande, a.k.a. The Hacker!@example.com'
       ]
+    end
 
-    INVALID_URLS = [
-        'http:// example.com', 'ftp://example.com',
-        'http://.com', 'http://', 'test', '...'
+    def invalid_urls
+      @invalid_urls = [
+        # Lifted from dm-validations specs
+        'http:// example.com', 
+        'ftp://example.com',
+        'http://.com', 
+        'http://', 
+        'test', 
+        '...'
       ]
+    end
 
     private
 
+      def validate
+        @message ||= '%s has an invalid format'.t(@attribute_name)
 
-      def validate_all( attr, context, opts )
-        opts[:allow_nil] = (val=opts.delete(:allow_nil)).nil? ? true : val
+        case @options[:allow_nil]
+          when nil, true ; validate_ok_values nil
+          else           ; validate_error_values nil
+        end
 
-        case opts[:as]
-          when :email_address
-            validate_email_address( attr, context, opts )
-          when :url 
-            validate_url( attr, context, opts )
-          else
-            validate_err_vals( attr, context, opts[:reject], opts )
+        case @options[:as]
+          when :email_address ; validate_email_address
+          when :url           ; validate_url
+          else                ; validate_reject_values
         end
       end
 
-      def validate_err_vals( attr, context, err_vals, opts )
-        err_msg = opts[:message] || '%s has an invalid format'.t(humanize(attr))
-
-        err_vals.each do |err_val|
-          err_msg_str = err_msg.is_a?(Proc) ? err_msg[err_val] : err_msg
-          validate_err_val( attr, err_val, context, 
-            %Q/should not be valid if :%s is "%s"/.t( attr, err_val ) )
-          validate_err_msg( attr, err_val, err_msg_str, context, 
-            %Q/should have error "%s" if :%s is "%s"/.t( err_msg_str, attr, err_val ) )
-        end
-
-        validate_nil_val( attr, context, opts[:allow_nil] )
+      def validate_reject_values
+        validate_error_values @options.delete(:reject)
       end
 
-      def validate_email_address( attr, context, opts )
-        # opts[:message] ||= lambda { |val| '%s is not a valid email address'.t(val) }
-        validate_err_vals( attr, context, INVALID_EMAILS, opts )
+      def validate_email_address
+        @options[:reject] = invalid_emails
+        validate_reject_values
       end
 
-      def validate_url( attr, context, opts )
-        # opts[:message] ||= lambda { |val| '%s is not a valid URL'.t(val) }
-        validate_err_vals( attr, context, INVALID_URLS, opts )
-      end
-
-      def validate_nil_val( attr, context, allow_nil )
-        if allow_nil
-          validate_ok_val attr, nil, context, "should be valid if :#{attr} is nil"
-        else
-          validate_err_val attr, nil, context, "should not be valid if :#{attr} is nil"
-        end
+      def validate_url
+        @options[:reject] = invalid_urls
+        validate_reject_values
       end
 
   end
