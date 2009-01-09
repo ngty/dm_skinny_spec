6acbc9722afc1a834547e445b27f9a4bdf55cbe8
NgTzeYang <ngty77@gmail.com>
2009-01-09 15:22:47 +0800

    Did tidying up.

--- lib/validations/it_should_validate_is_confirmed.rb
+++ lib/validations/it_should_validate_is_confirmed.rb
@@ -1,84 +1,79 @@
 module DmSkinnySpec::Validations::ItShouldValidateIsConfirmed
 
-  def it_should_validate_is_confirmed( attr, opts={} )
-    DmSkinnySpec::Validations::ItShouldValidateIsConfirmed.validate attr, opts, self
+  def it_should_validate_is_confirmed( attribute, options={} )
+    DmSkinnySpec::Validations::ItShouldValidateIsConfirmed.run attribute, options, self
   end
 
   class << self
 
     include DmSkinnySpec::Validations::Common
 
-    def validate_all( attr, context, opts )
-      err_msg = (opts[:message]||'%s does not match the confirmation').t(humanize(attr))
-      allow_nil = (val=opts[:allow_nil]).nil? ? true : val
-      confirm = opts[:confirm] || "#{attr}_confirmation".to_sym
+    def validate
+      @message ||= '%s does not match the confirmation'.t(@attribute_name)
+      @confirm = @options[:confirm] || "#{@attribute}_confirmation".to_sym
+      confirm, attribute = @confirm, @attribute
 
-      validate_err_val_and_err attr, confirm, context, err_msg
-      validate_nil_val_and_err attr, confirm, context, err_msg, allow_nil
-    end
+      value_proc = lambda { |instance,i|
+        '%s.%s'.t( (@orig_ok_value||=instance.attribute_get(attribute)), i )
+      }
 
-    def validate_err_val_and_err( attr, confirm, context, err_msg )
-      attr_err_val = lambda { |instance| '%s.0'.t(instance.attribute_get(attr)) }
-      confirm_err_val = lambda { |instance| '%s.1'.t(instance.send(confirm)) }
+      validate_ok_values value_proc, 'matches confirmation'
+      validate_error_values value_proc, 'does not match confirmation'
 
-      validate_err_val attr, confirm, attr_err_val, confirm_err_val, context,
-        "should not be valid if :#{attr} does not match confirmation"
-
-      validate_err_msg attr, confirm, attr_err_val, confirm_err_val, err_msg, context,
-        "should have error \"#{err_msg}\" if :#{attr} does not match confirmation"
-    end
-
-    def validate_nil_val_and_err( attr, confirm, context, err_msg, allow_nil )
-      if allow_nil
-        validate_ok_nil_val attr, confirm, context, 
-          "should be valid if :#{attr} and confirmation are nil"
-      else
-        validate_err_val attr, confirm, nil, nil, context, 
-          "should not be valid if :#{attr} and confirmation are nil"
-        validate_err_msg attr, confirm, nil, nil, err_msg, context, 
-          "should have error \"#{err_msg}\" if :#{attr} and confirmation are nil"
+      case @options[:allow_nil]
+        when true, nil ; validate_ok_values nil, 'is nil'
+        else           ; validate_error_values nil, 'is nil'
       end
     end
 
-    def validate_ok_nil_val( attr, confirm, context, example_str )
-      example.it example_str_for_context( example_str, context ) do
-        instance.attribute_set( attr, nil )
-        instance.send( "#{confirm}=", nil )
+    def validate_ok_value(value)
+      confirm, context, attribute = @confirm, @context, @attribute
+      description = "should be valid if :%s %s".t( attribute, humanize_condition(value) )
+
+      run_example description do
+        ok_val = value.is_a?(Proc) ? value : lambda{|a,b| value }
+        instance.attribute_set( attribute, ok_val[instance,0] )
+        instance.send( "#{confirm}=", ok_val[instance,0] )
         instance.valid?(context).should be_true
       end
     end
 
-    def validate_err_val( attr, confirm, attr_err_val, confirm_err_val, context, example_str )
-      example.it example_str_for_context( example_str, context ) do
-        ok_val = instance.attribute_get(attr)
-        attr_err_val = attr_err_val[instance] if attr_err_val.is_a?(Proc)
-        confirm_err_val = confirm_err_val[instance] if confirm_err_val.is_a?(Proc)
+    def validate_error_value(value)
+      confirm, context, attribute = @confirm, @context, @attribute
+      description = "should not be valid if :%s %s".t( attribute, humanize_condition(value) )
 
-        instance.attribute_set( attr, attr_err_val )
-        instance.send( "#{confirm}=", confirm_err_val )
+      run_example description do
+        ok_val  = instance.attribute_get(attribute)
+        err_val = value.is_a?(Proc) ? value : lambda{|a,b| value }
+
+        instance.attribute_set( attribute, err_val[instance,0] )
+        instance.send( "#{confirm}=", err_val[instance,1] )
         instance.valid?(context).should be_false
 
-        instance.attribute_set( attr, ok_val )
+        instance.attribute_set( attribute, ok_val )
         instance.send( "#{confirm}=", ok_val )
         instance.valid?(context).should be_true
       end
     end
 
-    def validate_err_msg( attr, confirm, attr_err_val, confirm_err_val, err_msg, context, example_str )
-      example.it example_str_for_context( example_str, context ) do
-        ok_val = instance.attribute_get(attr)
-        attr_err_val = attr_err_val[instance] if attr_err_val.is_a?(Proc)
-        confirm_err_val = confirm_err_val[instance] if confirm_err_val.is_a?(Proc)
+    def validate_error_message(value)
+      message, confirm, context, attribute = @message, @confirm, @context, @attribute
+      description = "should have error \"%s\" if :%s %s".t( message, attribute, humanize_condition(value) )
 
-        instance.attribute_set( attr, attr_err_val )
-        instance.send( "#{confirm}=", confirm_err_val )
+      run_example description do
+        ok_val  = instance.attribute_get(attribute)
+        err_val = value.is_a?(Proc) ? value : lambda{|a,b| value }
+        err_msg = message.is_a?(Proc) ? message : lambda{|_| message }
+
+        instance.attribute_set( attribute, err_val[instance,0] )
+        instance.send( "#{confirm}=", err_val[instance,1] )
         instance.valid?(context)
-        instance.errors[attr].should include(err_msg)
+        instance.errors[attribute].should include(err_msg[instance])
 
-        instance.attribute_set( attr, ok_val )
+        instance.attribute_set( attribute, ok_val )
         instance.send( "#{confirm}=", ok_val )
         instance.valid?(context)
-        instance.errors[attr].should be_nil
+        instance.errors[attribute].should be_nil
       end
     end
 
