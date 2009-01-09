bfea2d5bf0ffe10e809a9439db89b4d65a475e05
NgTzeYang <ngty77@gmail.com>
2009-01-09 17:03:06 +0800

    Moved the class description back to before() macro.

--- spec/validations/it_should_validate_absent_spec.rb
+++ spec/validations/it_should_validate_absent_spec.rb
@@ -1,30 +1,35 @@
 require 'pathname'
 require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
 
-class Page
-  include DataMapper::Resource
-
-  property :id,              Serial
-  property :first_unwanted,  String,  :auto_validation => false
-  property :second_unwanted, String,  :auto_validation => false
-  property :is_unwanted,     Boolean, :auto_validation => false
-
-  validates_absent :first_unwanted,  :when    => :first_unwanted_test
-  validates_absent :second_unwanted, :message => "We don't want 2nd unwanted"
-  validates_absent :is_unwanted   
-end
-
-Page.fix {{ 
-  :first_unwanted  => [ nil, '' ].pick,
-  :second_unwanted => [ nil, '' ].pick,
-  :is_unwanted     => [ nil, false ].pick
-}}
-
 describe 'it_should_validate_absent' do
 
   before do
+
+    class Page
+      include DataMapper::Resource
+
+      property :id,              Serial
+      property :first_unwanted,  String,  :auto_validation => false
+      property :second_unwanted, String,  :auto_validation => false
+      property :is_unwanted,     Boolean, :auto_validation => false
+
+      validates_absent :first_unwanted,  :when    => :first_unwanted_test
+      validates_absent :second_unwanted, :message => "We don't want 2nd unwanted"
+      validates_absent :is_unwanted   
+    end
+
+    Page.fix {{ 
+      :first_unwanted  => [ nil, '' ].pick,
+      :second_unwanted => [ nil, '' ].pick,
+      :is_unwanted     => [ nil, false ].pick
+    }}
+
     Page.auto_migrate!
     @instances = {}
+  end
+
+  after do
+    Object.send(:remove_const,'Page') 
   end
 
   def instance(id=:default)
--- spec/validations/it_should_validate_present_spec.rb
+++ spec/validations/it_should_validate_present_spec.rb
@@ -1,32 +1,37 @@
 require 'pathname'
 require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
 
-class User
-  include DataMapper::Resource
-
-  property :id,          Serial
-  property :name,        String,  :auto_validation => false
-  property :email,       String,  :auto_validation => false
-  property :address,     String,  :auto_validation => false
-  property :is_active,   Boolean, :auto_validation => false
-
-  validates_present :name, :when => :name_test
-  validates_present :email, :is_active
-  validates_present :address, :message => 'User cannot be homeless'
-end
-
-User.fix {{ 
-  :name      => 'Jimmy Page',
-  :email     => 'jimmy_page@fmail.com',
-  :is_active => [ true, false ].pick,
-  :address   => 'Hello World Avenue, Apt 989, Republic Mars'
-}}
-
 describe 'it_should_validate_present' do
 
   before do
+
+    class User
+      include DataMapper::Resource
+
+      property :id,          Serial
+      property :name,        String,  :auto_validation => false
+      property :email,       String,  :auto_validation => false
+      property :address,     String,  :auto_validation => false
+      property :is_active,   Boolean, :auto_validation => false
+
+      validates_present :name, :when => :name_test
+      validates_present :email, :is_active
+      validates_present :address, :message => 'User cannot be homeless'
+    end
+
+    User.fix {{ 
+      :name      => 'Jimmy Page',
+      :email     => 'jimmy_page@fmail.com',
+      :is_active => [ true, false ].pick,
+      :address   => 'Hello World Avenue, Apt 989, Republic Mars'
+    }}
+
     User.auto_migrate!
     @instances = {}
+  end
+
+  after do
+    Object.send(:remove_const,'User') 
   end
 
   def instance(id=:default)
