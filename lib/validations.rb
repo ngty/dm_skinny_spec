934f6f2c6cc7ac1acd42424ef183daabc3cf2ac2
NgTzeYang <ngty77@gmail.com>
2009-01-09 17:03:49 +0800

    Major tidying up.

--- lib/validations.rb
+++ lib/validations.rb
@@ -4,83 +4,118 @@ if Object.const_defined?('DataMapper') && DataMapper.const_defined?('Validate')
 
     module Common
 
-      def validate( attr, opts, example )
-        @example = example
-        validate_all attr, (opts.delete(:when)||:default), opts
+      def run( attribute, options, example_group )
+        @example_group  = example_group
+        @attribute      = attribute
+        @attribute_name = Extlib::Inflection.humanize(attribute.to_s)
+        @context        = options.delete(:when) || :default
+        @condition      = options.delete(:condition) # not yet supported
+        @message        = options.delete(:message)
+        @options        = options
+
+        validate
       end
 
       private
 
-        def example
-          @example
+        def run_example( description, &proc )
+          @example_group.it example_description_for_context(description), &proc
         end
 
-        def validate_ok_val( attr, ok_val, context, example_str )
-          example.it example_str_for_context( example_str, context ) do
-            instance.attribute_set( attr, ok_val.is_a?(Proc)?ok_val[self]:ok_val )
-            instance.valid?(context).should be_true
+        def validate_error_values( values, condition_description='is %s' )
+          @condition_description = condition_description
+          [values].flatten.each do |value|
+            validate_error_value   value
+            validate_error_message value
           end
         end
 
-        def validate_err_val( attr, err_val, context, example_str )
-          example.it example_str_for_context( example_str, context ) do
-            ok_val = instance.attribute_get(attr)
+        def validate_error_value(value)
+          context, attribute = @context, @attribute
+          description = "should not be valid if :%s %s".t( attribute, humanize_condition(value) )
 
-            instance.attribute_set( attr, err_val.is_a?(Proc)?err_val[self]:err_val )
+          run_example description do
+            ok_val  = instance.attribute_get(attribute)
+            err_val = value.is_a?(Proc) ? value : lambda{|_| value }
+
+            instance.attribute_set( attribute, err_val[self] )
             instance.valid?(context).should be_false
 
-            instance.attribute_set( attr, ok_val )
+            instance.attribute_set( attribute, ok_val )
             instance.valid?(context).should be_true
           end
         end
 
-        def validate_err_msg( attr, err_val, err_msg, context, example_str )
-          example.it example_str_for_context( example_str, context ) do
-            ok_val = instance.attribute_get(attr)
+        def validate_error_message(value)
+          context, attribute, message = @context, @attribute, @message
+          description = "should have error \"%s\" if :%s %s" %
+            [ message, attribute, humanize_condition(value) ]
 
-            instance.attribute_set( attr, err_val.is_a?(Proc)?err_val[self]:err_val )
-            instance.valid?(context)
-            instance.errors[attr].should include(err_msg.is_a?(Proc)?err_msg[err_val]:err_msg)
+          run_example description do
+            ok_val  = instance.attribute_get(attribute)
+            err_val = value.is_a?(Proc) ? value : lambda{|_| value }
+            err_msg = message.is_a?(Proc) ? message : lambda{ |_| message }
 
-            instance.attribute_set( attr, ok_val )
+            instance.attribute_set( attribute, err_val[self] )
             instance.valid?(context)
-            instance.errors[attr].should be_nil
+            instance.errors[attribute].should include(err_msg[self])
+
+            instance.attribute_set( attribute, ok_val )
+            instance.valid?(context)
+            instance.errors[attribute].should be_nil
           end
         end
 
-        def example_str_for_context( example_str, context )
-          example_str += " (for :#{context})" unless context == :default
-          example_str
+        def validate_ok_values( values, condition_description='is %s' )
+          @condition_description = condition_description
+          [values].flatten.each do |value|
+            validate_ok_value value
+          end
         end
 
-        def humanize(arg)
-          case arg
-            when Symbol ; Extlib::Inflection.humanize(arg.to_s)
-            when nil    ; 'nil'
-            when String ; '"%s"'.t(arg)
-            else        ; arg.to_s
+        def validate_ok_value(value)
+          context, attribute = @context, @attribute
+          description = "should be valid if :%s %s".t( attribute, humanize_condition(value) )
+
+          run_example description do
+            ok_val = value.is_a?(Proc) ? value : lambda{|_| value }
+            instance.attribute_set( attribute, ok_val[self] )
+            instance.valid?(context).should be_true
           end
+        end
+
+        def example_description_for_context(description)
+          description += " (for :#{@context})" unless @context == :default
+          description
+        end
+
+        def humanize_condition(value)
+          @condition_description %
+            case value
+              when nil    ; 'nil'
+              when String ; '"%s"'.t(value)
+              else        ; value.to_s
+            end
         end
 
     end
   end
 
-
-  require 'validations/it_should_validate_present'
   require 'validations/it_should_validate_absent'
-  require 'validations/it_should_validate_is_accepted'
-  require 'validations/it_should_validate_is_unique'
-  require 'validations/it_should_validate_is_confirmed'
-  require 'validations/it_should_validate_length'
+  require 'validations/it_should_validate_present'
   require 'validations/it_should_validate_format'
+  require 'validations/it_should_validate_length'
+  require 'validations/it_should_validate_is_accepted'
+  require 'validations/it_should_validate_is_confirmed'
+  require 'validations/it_should_validate_is_unique'
 
-  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidatePresent)
   Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateAbsent)
-  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateIsAccepted)
-  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateIsUnique)
-  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateIsConfirmed)
-  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateLength)
+  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidatePresent)
   Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateFormat)
+  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateLength)
+  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateIsAccepted)
+  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateIsConfirmed)
+  Spec::Example::ExampleGroup.extend(DmSkinnySpec::Validations::ItShouldValidateIsUnique)
 
 end
 
